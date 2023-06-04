4e334f5c5495a2dcb8d2800dfee996d3acee452e
raskolnikov104 <mahatabtamal@gmail.com>
2023-06-04 22:52:08 +0600

    startup reduced to half

--- init.el
+++ init.el
@@ -1,10 +1,13 @@
+
 (recentf-mode 1)    ;;remembers of recent files
 (setq history-length 25)
 (savehist-mode 1)    ;;minibuffer history is saved
-(save-place-mode 1)    ;;remembers previous cursor position in a file
+(save-place-mode 1);;remembers previous cursor position in a file
+(global-page-break-lines-mode)
+(setq auto-save-default t)
 ;;(delete-selection-mode t)
 
-(require 'package)   
+(require 'package) 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
@@ -13,9 +16,12 @@
 ;;   (package-refresh-contents)
 ;;   (package-install 'use-package))
 
+(setq use-package-always-defer t)
+
 (use-package sudo-edit)    ;;allows editing in root directory
 				
-(use-package marginalia   ;; M-x details information
+(use-package marginalia;; M-x details information
+  :after vertico
   :custom
   (marginalia-align 'left)    ;; info aligns to left/right/center
   :init    ;; starts
@@ -29,16 +35,17 @@
 (global-set-key (kbd "C-x W") 'which-key-show-top-level)    ;; instantly show which-key menu
 
 (use-package tree-sitter
+  :demand t
   :config
   (require 'tree-sitter-langs)
-(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+(add-hook 'tree-sitter-after-on-hook 'tree-sitter-hl-mode))
 
 (add-hook 'c-mode-hook #'tree-sitter-mode)
 (add-hook 'c++-mode-hook #'tree-sitter-mode)
 (add-hook 'java-mode-hook #'tree-sitter-mode)
 
 (use-package eglot)
-(add-to-list 'eglot-server-programs '((c++-mode c-mode)))
+;;(add-to-list 'eglot-server-programs '((c++-mode java-mode)))
 ;;(add-hook 'c-mode-hook 'eglot-ensure)
 (add-hook 'c++-mode-hook 'eglot-ensure)
 (add-hook 'java-mode-hook 'eglot-ensure)
@@ -46,16 +53,16 @@
 (use-package eglot-java)
 (add-hook 'java-mode-hook 'eglot-java-mode)
 (add-hook 'eglot-java-mode-hook (lambda ()                                        
-  (define-key eglot-java-mode-map (kbd "C-c l n") #'eglot-java-file-new)
-  (define-key eglot-java-mode-map (kbd "C-c l x") #'eglot-java-run-main)
-  (define-key eglot-java-mode-map (kbd "C-c l t") #'eglot-java-run-test)
-  (define-key eglot-java-mode-map (kbd "C-c l N") #'eglot-java-project-new)
-  (define-key eglot-java-mode-map (kbd "C-c l T") #'eglot-java-project-build-task)
-  (define-key eglot-java-mode-map (kbd "C-c l R") #'eglot-java-project-build-refresh)))
+  (define-key eglot-java-mode-map (kbd "C-c l n") 'eglot-java-file-new)
+  (define-key eglot-java-mode-map (kbd "C-c l x") 'eglot-java-run-main)
+  (define-key eglot-java-mode-map (kbd "C-c l t") 'eglot-java-run-test)
+  (define-key eglot-java-mode-map (kbd "C-c l N") 'eglot-java-project-new)
+  (define-key eglot-java-mode-map (kbd "C-c l T") 'eglot-java-project-build-task)
+  (define-key eglot-java-mode-map (kbd "C-c l R") 'eglot-java-project-build-refresh)))
 
 
 (use-package vertico
-  :ensure t
+  :demand t
   :init
   (vertico-mode)
   (setq vertico-scroll-margin 0) ;; scroll margin
@@ -73,7 +80,8 @@
   ;; Tidy shadowed file names
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
 
-(use-package orderless  ;;completion algorithm taht matches by any order
+(use-package orderless    ;;completion algorithm taht matches by any order
+  :after marginalia
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
@@ -90,27 +98,38 @@
 ;;  ("M-s l" . consult-line))    ;; better C-s search
 
 (global-set-key (kbd "C-s") 'consult-line)
-(global-set-key (kbd "C-x a f") 'affe-find)
+(use-package affe
+  :defer t
+  :bind
+  ("C-x a f" . affe-find))
 	     
 (use-package corfu    ;; completion ui
-  :custom
-  (corfu-cycle t)    ;; cycles the completion options
-  (corfu-auto t)    ;; autocompletion
-  (corfu-popupinfo-mode t)
-  (corfu-min-width 30)    ;; ui minimum width for 30 chars
-  (corfu-scroll-margin 5)    ;; scroll margin
-;;  (corfu-history-mode t)    ;; saves comp. option history
+ :custom
+ (corfu-cycle t)    ;; cycles the completion options
+ (corfu-auto t)    ;; autocompletion
+ (corfu-popupinfo-mode t)
+ (corfu-min-width 20)    ;; ui minimum width for 30 chars
+ (corfu-scroll-margin 5)    ;; scroll margin
+ (corfu-history-mode t)    ;; saves comp. option history
   :init
   (global-corfu-mode))
 
 (use-package highlight-indent-guides
+  :defer t
   :config
+  (setq highlight-indent-guides-method 'column)
   (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
 
+(use-package multiple-cursors
+  :defer t)
+(global-set-key (kbd "C-S-e C-S-e") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+;;(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 (add-hook 'org-mode-hook (lambda ()
 			   (setq-local sentence-end-double-space nil)))    ;; disables double space end in org mode
-;;(setq make-backup-files nil)    ;; stops making temporary backup files
+(setq make-backup-files nil)    ;; stops making temporary backup files
 (setq delete-by-moving-to-trash t)    ;; sends deleted items to trash instead of permanent delete
 
 ;; (use-package emacs
@@ -126,29 +145,27 @@
 
 (setq native-comp-async-report-warnings-errors nil)    ;; hides the error reports for nativecomp build
 
-
-(global-page-break-lines-mode)    ;;draws seamless horizontal lines
-;;(setq frame-title-format nil)
-
-;;(setq frame-resize-pixelwise t)
+(setq frame-resize-pixelwise t)    ;; speeds up startup
 
 (use-package gcmh    ;; Using garbage magic hack
-    :config
-   (gcmh-mode 1))
+  :demand t
+  :config
+  (gcmh-mode 1))
 
 (setq gc-cons-threshold 402653184    ;;  Setting garbage collection threshold
       gc-cons-percentage 0.6)
 
-
-(add-hook 'after-init-hook #'flymake-mode)
+;; (add-hook 'prog-mode-hook 'flymake-mode)
 
 (use-package doom-themes
+  :demand t
   :config
   ;;  Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-palenight t)    ;; loads catppuccin
-  (doom-themes-visual-bell-config)      ;;  Enable flashing mode-line on errors
+  (load-theme 'doom-palenight t)    ;; loads doom-theme
+  (doom-themes-visual-bell-config)    ;; Enable flashing mode-line on errors
+  (doom-themes-neotree-config)    ;;  variable in neotree
  ;; (setq catppuccin-flavor 'macchiato)    ;; says it already
  ;;  (catppuccin-reload)     ;; reloads the macchiato variant, shit
 (doom-themes-org-config))    ;; that's why I use this bloat
@@ -182,7 +199,8 @@
   :hook (after-init . doom-modeline-mode))
 
 (setq custom-file "~/.emacs.d/custom.el")    ;; trashes custom settings to this path
-(load custom-file)
+(load custom-file
+      :defer t)
 
 ;; basic ui tweaks that come builtin.
 (setq inhibit-startup-message t)    ;stops the default startup screen from appearing
@@ -191,16 +209,17 @@
 (scroll-bar-mode -1)
 ;;(add-to-list 'default-frame-alist '(fullscreen . maximized))    ;; starts in fullscreen
 
-(global-display-line-numbers-mode 1)  ;; shows line numbers everywhere
-(setq-default line-spacing 0.2)     ;; spacing between lines
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+;;(global-display-line-numbers-mode 1)  ;; shows line numbers everywhere
 
-(use-package all-the-icons)
+(use-package all-the-icons
+  :demand t)
 
 (use-package all-the-icons-completion
   :after (marginalia all-the-icons)
   :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
   :init
-  (all-the-icons-completion-mode))
+ (all-the-icons-completion-mode))
 
 (add-hook 'prog-mode-hook 'hl-line-mode)
 (pixel-scroll-precision-mode)  ;; smooth scrolling :0
@@ -210,23 +229,31 @@
   :after corfu
   :custom
   (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
-  :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+  (add-to-list 'corfu-margin-formatters 'kind-icon-margin-formatter))
 
 
 (use-package all-the-icons-dired)     ;; icons for dired
 (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
-(add-hook 'dired-mode-hook 'dired-git-mode)
+;;(add-hook 'dired-mode-hook 'dired-git-mode)
+
 (use-package dired-sidebar
+  :defer t
   :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
   :commands (dired-sidebar-toggle-sidebar)
   :init
   (add-hook 'dired-sidebar-mode-hook
             (lambda ()
               (unless (file-remote-p default-directory)
-                (auto-revert-mode)))))
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
 
-(setq inhibit-compacting-font-caches t)
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'icons)
+  (setq dired-sidebar-use-term-integration t))
+
+;;(setq inhibit-compacting-font-caches t)
 
 (use-package dashboard
   :init
@@ -236,20 +263,19 @@
   (setq dashboard-startup-banner "~/.emacs.d/xemacs_color.svg")    ;; dashboard image
   (setq dashboard-banner-logo-title "Welcome to Emacs")    ;; welcome message
   (setq dashboard-items '((bookmarks . 3)    ;; items
-			  (recents . 3)))
+			  (agenda . 3)))
   (setq dashboard-display-icons-p t)
   (setq dashboard-modify-heading-icons '((bookmarks . "book")    ;; items icons
 				    (agenda . "calendar")))
-  ;; footer
-  (setq dashboard-set-footer t
-	dashboard-footer-icon
-	(all-the-icons-fileicon "emacs"
-       				:height 1
-				:v-adjust -0.1
-				:face 'font-lock-string-face))
+  ;; ;; footer
+  ;; (setq dashboard-set-footer t
+  ;; 	dashboard-footer-icon
+  ;; 	(all-the-icons-fileicon "emacs"
+  ;;      				:height 1
+  ;; 				:v-adjust -0.1
+  ;; 				:face 'font-lock-string-face))
   (dashboard-setup-startup-hook))
 (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))    ;; init buffer
-
 (setq org-agenda-span 'month)    ;; views org agenda for the whole month
 
 (setq org-todo-keywords    ;; todo list keywords
@@ -265,9 +291,9 @@
 
 (global-set-key (kbd "C-c i t") 'org-insert-todo-heading)    ;; insert todo heading, easier than switching
 
-(setq org-agenda-files '("~/Library/org/agenda.org"    ;;  org agenda files list
-			 "~/Library/org/university.org"
-			 "~/Library/org/programming.org"
+(setq org-agenda-files '("~/Library/org/tasks.org"    ;;  org agenda files list
+			 "~/Library/org/uni.org"
+			 "~/Library/org/coding.org"
 			 "~/Library/org/books.org"))
 (setq org-agenda-include-diary nil)    ;; exclude the holiday shit
 
@@ -286,10 +312,10 @@
   :font "Recursive Casual"
   :height 110
   :weight 'regular)
-(set-face-attribute 'fixed-pitch nil     ;; maybe documantation font
-  :font "Rec Mono Casual"
-  :height 110
-  :weight 'regular)
+;; (set-face-attribute 'fixed-pitch nil     ;; maybe documantation font
+;;   :font "Rec Mono Custom"
+;;   :height 120
+;;   :weight 'regular)
 (set-fontset-font "fontset-default" 'bengali    ;; bengali font
 		  (font-spec :family "Hind Siliguri"
 			     :size 16))
@@ -305,9 +331,6 @@
 	  ;; ("#+begin_src" . "🛈")
 	  ;; ("#+end_src" . "🛈")))
   (prettify-symbols-mode 1))
-
-
-
 (add-hook 'org-mode-hook 'my-pretty-symbols)
 
 (use-package markdown-mode
@@ -324,3 +347,23 @@
 (use-package org-superstar     ;; fancy org heading icons
 :config
 (add-hook 'org-mode-hook 'org-superstar-mode))
+
+(use-package ligature
+  :config
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                       "\\\\" "://"))
+  ;; Enables ligature checks globally in all buffers.  You can also do it
+  ;; per mode with `ligature-mode'.
+  (global-ligature-mode t))
